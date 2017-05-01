df1381ce06572cdd6c2f2205191ee7a8bfaf1e33
Matthew Beck <beckmc@lafayette.edu>
2017-04-30 20:22:51 -0400

    Distance Vector
    Can add items to the distance vector. Code is pretty
    straightforward/simple. Key is made to be easily parsed for future use

--- DistanceVector.java
+++ DistanceVector.java
@@ -1,7 +1,7 @@
 import java.util.HashMap;
 /**
  * A hashmap of the different distances to the other nodes within the network of Routers.
- * 
+ *
  * @author Matthew Becka and Chris VanBlargan
  */
 public class DistanceVector
@@ -15,18 +15,34 @@ public class DistanceVector
     public DistanceVector()
     {
         // initialise instance variables
-        
+
     }
 
     /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
+     * Add item to the router's distance vector
+     *
+     * @param  ipAddress  the ipAddress of a router
+     * @param  port  the port of a router
+     * @param  weighet the weight to get to that router from the current one
+     * @return     true if added to the distance vector
      */
     public boolean add(String ipAddress, int port, int weight)
     {
-        //dv.put?
-        return true;
+        //convert ipAddress, port into one identifying item
+        //key is in the form of ipAddress:port#
+        String key = ipAddress + ":" + Integer.toString(port);
+
+        //place item into hash map
+        //if already exists, only place if weight is smaller
+        if (dV.get(key) != null) {
+          if (dV.get(key) > weight) {
+            dV.put(key, weight);
+            return true;
+          }
+        } else {
+          dV.put(key, weight);
+          return true;
+        }
+        return false;
     }
 }
