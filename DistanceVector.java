d3d17469e9a1a892ff7972d9492a0ce41f59e4db
Matthew Beck <beckmc@lafayette.edu>
2017-05-03 17:49:22 -0400

    More Hash Map Updates
    Made sure this compiles. Can now add neighbors, setup to code for when
    entire distance vectors are received.

--- DistanceVector.java
+++ DistanceVector.java
@@ -2,12 +2,13 @@ import java.util.HashMap;
 /**
  * A hashmap of the different distances to the other nodes within the network of Routers.
  *
- * @author Matthew Becka and Chris VanBlargan
+ * @author Matthew Beck and Chris VanBlargan
  */
 public class DistanceVector
 {
-    // instance variables - replace the example below with your own
-    private HashMap dV;
+    // the distance vector is stored as a hashmap. The key is the ip address and port number in one string
+    // the value stored is the weight and the next node on the path to get there
+    private HashMap<String, String> dV;
 
     /**
      * Constructor for objects of class DistanceVector
@@ -19,30 +20,49 @@ public class DistanceVector
     }
 
     /**
-     * Add item to the router's distance vector
+     * Add item to the router's distance vector. Meant to be called upon initial setup of neighbors.
      *
      * @param  ipAddress  the ipAddress of a router
      * @param  port  the port of a router
-     * @param  weighet the weight to get to that router from the current one
+     * @param  weight the weight to get to that router from the current one
      * @return     true if added to the distance vector
      */
-    public boolean add(String ipAddress, int port, int weight)
+    public boolean addNeighbor(String ipAddress, int port, int weight)
     {
         //convert ipAddress, port into one identifying item
         //key is in the form of ipAddress:port#
+        //value is in the form of weight:nextNode
+        //value is only used here if the distance to neighbor is shorter than an already existing path
         String key = ipAddress + ":" + Integer.toString(port);
+        String value = Integer.toString(weight) + ":" + key;
 
         //place item into hash map
         //if already exists, only place if weight is smaller
         if (dV.get(key) != null) {
-          if (dV.get(key) > weight) {
-            dV.put(key, weight);
+          //pulls the integer value of the weight of the old entry in the hashmap
+          int oldWeight = Integer.parseInt(dV.get(key).split(":")[0]);
+
+          //new weight is smaller, update
+          if (weight < oldWeight) {
+            dV.put(key, value);
             return true;
           }
         } else {
-          dV.put(key, weight);
+          //no entry exists for neighbor, place in hashmap
+          dV.put(key, value);
           return true;
         }
+        //entry was not added
         return false;
+    }
+
+   /**
+    *
+    * @param vector the dstance vector receieved by the router to update
+    * @param sender the router who sent the distance vector
+    * @return true when distance vector is finished updating
+    */
+    public boolean addVector(DistanceVector vector, String sender) {
+      return true;
     }
 }
--- Router.java
+++ Router.java
@@ -5,106 +5,104 @@ import java.util.Scanner;
 import java.io.FileReader;
 
 /**
- * Write a description of class Router here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+* Write a description of class Router here.
+*
+* @author (your name)
+* @version (a version number or a date)
+*/
 public class Router
 {
-    // instance variables - replace the example below with your own
-    private String ipAddress;
-    private int portNumber;
-    private DistanceVector distV;
+  // instance variables
+  private String ipAddress;
+  private int portNumber;
+  private DistanceVector distV;
 
-    /**
-     * Constructor for objects of class Router
-     */
-    public Router(boolean reverse, String filename)
-    {
-        // initialise instance variables
-        if(reverse){       //if poisoned reverse
-        
-        }else{              //no poisoned reverse
-        
-        }
-        //read file and convert to DistanceVector Object
-        BufferedReader br = null;
-		FileReader fr = null;
-		
-		try {
+  private boolean poisonedReverse;
 
-			fr = new FileReader(filename);
-			br = new BufferedReader(fr);
+  /**
+  * Constructor for objects of class Router
+  */
+  public Router(boolean reverse, String filename)
+  {
+    // initialise instance variables
+    poisonedReverse = reverse;
+    //read file and convert to DistanceVector Object
+    BufferedReader br = null;
+    FileReader fr = null;
 
-			String sCurrentLine;
+    try {
 
-			if ((sCurrentLine = br.readLine()) != null) {    //parses first line for this router's info
-				System.out.println(sCurrentLine);
-				String[] parts = sCurrentLine.split(" ");
-				ipAddress = parts[0];
-				portNumber = Integer.parseInt(parts[1]);
-			}
-			
-			while ((sCurrentLine = br.readLine()) != null) {  //parses rest of file for distance vector info
-				System.out.println(sCurrentLine);
-				String[] parts = sCurrentLine.split(" ");
-				int tempPortNumber = Integer.parseInt(parts[1]);
-				int tempWeight = Integer.parseInt(parts[2]);
-				distV.add(parts[0], tempPortNumber, tempWeight);
-			}
+      fr = new FileReader(filename);
+      br = new BufferedReader(fr);
 
-		} catch (IOException e) {
+      String sCurrentLine;
 
-			e.printStackTrace();
-
-		} finally {
-
-			try {
-
-				if (br != null)
-					br.close();
-
-				if (fr != null)
-					fr.close();
-
-			} catch (IOException ex) {
-
-				ex.printStackTrace();
-
-			}
-
-		}
-
-	}
-    
-    
-    public static void main(String[] args) throws Exception
-    {
-        if(args.length != 1){
-            System.out.println("Please specify a command: PRINT, MSG<dst-ip> <dst-port> <msg>, or CHANGE <dst-ip> <dst-port> <new-weight>");
-            System.exit(1);
-        }
-        if(args.length == 1 && args[0].equals("PRINT")){
-            System.out.println("Print");
-        }
-        if(args.length == 4 && args[0].equals("MSG")) {
-            System.out.println("Message");
-        }
-        if(args.length == 4 && args[0].equals("CHANGE")){
-            System.out.println("Change");
-        }
+      if ((sCurrentLine = br.readLine()) != null) {    //parses first line for this router's info
+      System.out.println(sCurrentLine);
+      String[] parts = sCurrentLine.split(" ");
+      ipAddress = parts[0];
+      portNumber = Integer.parseInt(parts[1]);
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int print(int y)
-    {
-        // put your code here
-        return  y;
+    while ((sCurrentLine = br.readLine()) != null) {  //parses rest of file for distance vector info
+      System.out.println(sCurrentLine);
+      String[] parts = sCurrentLine.split(" ");
+      int tempPortNumber = Integer.parseInt(parts[1]);
+      int tempWeight = Integer.parseInt(parts[2]);
+      distV.add(parts[0], tempPortNumber, tempWeight);
     }
+
+  } catch (IOException e) {
+
+    e.printStackTrace();
+
+  } finally {
+
+    try {
+
+      if (br != null)
+      br.close();
+
+      if (fr != null)
+      fr.close();
+
+    } catch (IOException ex) {
+
+      ex.printStackTrace();
+
+    }
+
+  }
+
+}
+
+
+public static void main(String[] args) throws Exception
+{
+  if(args.length != 1){
+    System.out.println("Please specify a command: PRINT, MSG<dst-ip> <dst-port> <msg>, or CHANGE <dst-ip> <dst-port> <new-weight>");
+    System.exit(1);
+  }
+  if(args.length == 1 && args[0].equals("PRINT")){
+    System.out.println("Print");
+  }
+  if(args.length == 4 && args[0].equals("MSG")) {
+    System.out.println("Message");
+  }
+  if(args.length == 4 && args[0].equals("CHANGE")){
+    System.out.println("Change");
+  }
+}
+
+/**
+* An example of a method - replace this comment with your own
+*
+* @param  y   a sample parameter for a method
+* @return     the sum of x and y
+*/
+public int print(int y)
+{
+  // put your code here
+  return  y;
+}
 }
